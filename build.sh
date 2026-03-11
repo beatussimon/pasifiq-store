49c16f7cb4c7d962d8acb57c514b73b80c0c801f
bea <beatussimon47@gmail.com>
2026-03-11 19:44:38 +0300

    Update the build.sh file to collect static

--- build.sh
+++ build.sh
@@ -2,7 +2,12 @@
 # exit on error
 set -o errexit
 
+# Install dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+# Collect static files (includes CSS updates)
+python manage.py collectstatic --no-input --clear
+
+# Run database migrations
 python manage.py migrate
